6e0ceb4e40aa67cddaf9181631e38fdfc5a6da90
rsevilla <rsevilla2026@alu.uct.cl>
2026-09-06 19:29:36 +0200

    Implement in-memory Vehiculo repository

---
+++ src/proyectobackend/repositories/vehiculo_repository.py
@@ -0,0 +1,25 @@
+from typing import List, Optional
+from proyectobackend.domain.vehiculo import Vehiculo
+
+class VehiculoRepository:
+    def __init__(self):
+        self._vehiculos: dict[int, Vehiculo] = {}
+        self._contador_id: int = 1
+
+    def crear(self, vehiculo: Vehiculo) -> Vehiculo:
+        vehiculo.id = self._contador_id
+        self._vehiculos[self._contador_id] = vehiculo
+        self._contador_id += 1
+        return vehiculo
+
+    def listar(self) -> List[Vehiculo]:
+        return list(self._vehiculos.values())
+
+    def buscar_por_id(self, vehiculo_id: int) -> Optional[Vehiculo]:
+        return self._vehiculos.get(vehiculo_id)
+
+    def buscar_por_patente(self, patente: str) -> Optional[Vehiculo]:
+        for vehiculo in self._vehiculos.values():
+            if vehiculo.patente == patente:
+                return vehiculo
+        return None
